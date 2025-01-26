c729cad2b6cbfddd5d3ec4b04d7789739d45f4c7
NightmareFlux <157146699+NightmareFlux@users.noreply.github.com>
2025-01-26 12:26:58 +0100

    morning
    continue

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,69 +1,56 @@
 extends CharacterBody2D
-
+@onready var anim = $AnimatedSprite2D
 const speed = 100
 var current_dir = "none"
 
 func _ready():
-	$AnimatedSprite2D.play("idle")
-
-func _physics_process(delta):
-	player_movement(delta)
+	anim.play("idle")
 
 func player_movement(_delta):
 	if Input.is_action_pressed("move_right"):
-		play_anim(1)
-		current_dir = "right"
+		play_anim(1, "right")
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("move_left"):
-		play_anim(1)
-		current_dir = "left"
+		play_anim(1, "left")
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("move_down"):
-		play_anim(1)
-		current_dir = "down"
+		play_anim(1, "down")
 		velocity.y = speed
 		velocity.x = 0
 	elif Input.is_action_pressed("move_up"):
-		play_anim(1)
-		current_dir = "up"
+		play_anim(1, "up")
 		velocity.y = -speed
 		velocity.x = 0
 	else:
-		play_anim(0)
+		play_anim(0, "up")
 		velocity.x = 0
 		velocity.y = 0
 
 	move_and_slide()
 
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
-	
+func play_anim(movement, dir):
+
 	if dir == "right":
 		anim.flip_h = false
-		if movement == 1:
-			anim.play("walk")
-		elif movement == 0:
-			anim.play("idle")
-			
+		anim.play("walk")
+		print(dir)
+
 	if dir == "left":
 		anim.flip_h = true
-		if movement == 1:
-			anim.play("walk")
-		elif movement == 0:
-			anim.play("idle")
-			
+		anim.play("walk")
+		print(dir)
+
 		if dir == "up":
 			anim.flip_h = true
-		if movement == 1:
-			anim.play("walk")
-		elif movement == 0:
-			anim.play("idle")
-			
-		if dir == "down":
-			anim.flip_h = false
+			if movement == 1:
+				anim.play("walk")
+			elif movement == 0:
+				anim.play("idle")
+
+	if dir == "down":
+		anim.flip_h = false
 		if movement == 1:
 			anim.play("walk")
 		elif movement == 0:
